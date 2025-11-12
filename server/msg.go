e632e93da0d838662a07d7881cf443e814de653c
Ky Anh Vo <tommyvo0406@gmail.com>
2025-11-12 14:10:22 -0500

    msg: added field closeconn for UpdateMsg

--- server/msg.go
+++ server/msg.go
@@ -10,6 +10,7 @@ type UpdateMsg struct {
 	ClientID		uint8	// partial key defined also by RoomID
 	// delete [CursorPos, CursorPos + DeleteLen - 1]
 	// then add InsertStr at CursorPos
+	closeconn		byte
 	CursorPos		uint64
 	DeleteLen		uint64
 	InsertLen		uint64
