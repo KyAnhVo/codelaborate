261251b832b5b0810f7c396904c5677f7f7b5158
Ky Anh Vo <tommyvo0406@gmail.com>
2025-11-30 10:44:22 -0600

    Revert "loop until all data is read"
    This reverts commit 386d5978b033dc0bd12a55f48b54bb24ab8df008.

--- client/src/network.cpp
+++ client/src/network.cpp
@@ -81,26 +81,24 @@ void Network::sendEntryMsg(EntryMsg msg) {
 
 void Network::recvMsg() {
     qDebug() << "Enter recvMsg()";
+    quint64 readData = 0;
+    UpdateMsg msg;
+    char buf;
 
-    while (this->socket.bytesAvailable() > 0) {
-        UpdateMsg msg;
-        char buf;
-
-        this->socket.read(&buf, 1);
-        switch (buf) { // not convert this to detect faulty msg status (4 to 7 might appear?)
-            case static_cast<char>(MsgStatus::ENTRY_ERR):
-            case static_cast<char>(MsgStatus::ENTRY_OK):
-                this->recvEntryMsg(buf);
-                break;
-            case static_cast<char>(MsgStatus::CLOSE_CONN):
-            case static_cast<char>(MsgStatus::UPDATE):
-                this->recvUpdateMsg(buf);
-                break;
-            default:
-                emit this->bogusSignal();
-        }
-        qDebug() << "Exit recvMsg()";
+    readData = this->socket.read(&buf, 1);
+    switch (buf) { // not convert this to detect faulty msg status (4 to 7 might appear?)
+        case static_cast<char>(MsgStatus::ENTRY_ERR):
+        case static_cast<char>(MsgStatus::ENTRY_OK):
+            this->recvEntryMsg(buf);
+            break;
+        case static_cast<char>(MsgStatus::CLOSE_CONN):
+        case static_cast<char>(MsgStatus::UPDATE):
+            this->recvUpdateMsg(buf);
+            break;
+        default:
+            emit this->bogusSignal();
     }
+    qDebug() << "Exit recvMsg()";
 }
 
 void Network::recvUpdateMsg(char msgStatus) {
