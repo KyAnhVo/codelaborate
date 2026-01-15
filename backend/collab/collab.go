1b1cf21d7999d0ada8dfed6dae40171bafddebbd
Your Name <you@example.com>
2026-01-14 22:56:23 -0600

    ...

--- backend/collab/collab.go
+++ backend/collab/collab.go
@@ -1,10 +1,28 @@
 package collab
 
 import (
+	"net/http"
+
 	gin "github.com/gin-gonic/gin"
 	ws "github.com/gorilla/websocket"
 )
 
-func SetupCollabRoomServer(r *gin.Engine) {
-	
+var upgrader = ws.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func SetupCollabRoomServer(r *gin.Context) {
+	conn, err := upgrader.Upgrade(r.Writer, r.Request, nil)
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	for {
+
+	}
 }
